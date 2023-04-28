98674e79500d1b76094ecfe8c89c7ef15e10b344
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-28 14:43:03 -0400

    Refactored to remove xIsNext useState. Not needed now that we are tracking currentMove

--- tic-tac-toe/src/Game.jsx
+++ tic-tac-toe/src/Game.jsx
@@ -2,9 +2,9 @@ import Board from "./Board";
 import { useState } from "react";
 
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([new Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
     const currentSquares = history[currentMove]; //accessing last element in history array so that currentSquares would have an array of current game
     
 
@@ -13,13 +13,11 @@ export default function Game() {
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
         //setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
     }
 
 
     function jumpTo(nextMove) {
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 === 0);
     }
 
    const moves = history.map((squares, move) => {
