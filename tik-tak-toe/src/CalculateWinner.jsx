ada672f27c69aaea872980362ab16834d787cdec
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-27 21:35:31 -0400

    Refactor

--- tik-tak-toe/src/CalculateWinner.jsx
+++ tik-tak-toe/src/CalculateWinner.jsx
@@ -1,24 +1,59 @@
-
 export default function calculateWinner(nSquare) {
-    //check rows, [0, 1 , 2] [3, 4, 5] [ 6, 7 ,8]
-    let theWinner = "";
- 
-    if (nSquare[0] == nSquare[1] && nSquare[1] == nSquare[2] && nSquare[0] != null) theWinner = nSquare[0];
-    else if (nSquare[3] == nSquare[4] && nSquare[4] == nSquare[5] && nSquare[3] != null) theWinner = nSquare[3];
-    else if (nSquare[6] == nSquare[7] && nSquare[7] == nSquare[8] && nSquare[6] != null) theWinner = nSquare[6];
-   
-    
+  //check rows, [0, 1 , 2] [3, 4, 5] [ 6, 7 ,8]
+  let theWinner = "";
+
+  if (
+    nSquare[0] == nSquare[1] &&
+    nSquare[1] == nSquare[2] &&
+    nSquare[0] != null
+  )
+    theWinner = nSquare[0];
+  else if (
+    nSquare[3] == nSquare[4] &&
+    nSquare[4] == nSquare[5] &&
+    nSquare[3] != null
+  )
+    theWinner = nSquare[3];
+  else if (
+    nSquare[6] == nSquare[7] &&
+    nSquare[7] == nSquare[8] &&
+    nSquare[6] != null
+  )
+    theWinner = nSquare[6];
   //check columns [0, 3, 6] [1, 4, 7] [2, 5, 8]
-    else if (nSquare[0] == nSquare[3] && nSquare[3] == nSquare[6] && nSquare[0] != null) theWinner = nSquare[0];
-    else if (nSquare[1] == nSquare[4] && nSquare[4] == nSquare[7] && nSquare[1] != null) theWinner = nSquare[1];
-    else if (nSquare[2] == nSquare[5] && nSquare[5] == nSquare[8] && nSquare[2] != null) theWinner = nSquare[2];
-    //check diangonals [0, 4, 8] and [2, 4, 6]
-    else if (nSquare[0] == nSquare[4] && nSquare[4] == nSquare[8] && nSquare[0] != null) theWinner = nSquare[0];
-    else if (nSquare[2] == nSquare[4] && nSquare[4] == nSquare[6] && nSquare[2] != null) theWinner = nSquare[2];
- 
- 
-    if (theWinner != "")
-      return  theWinner;
-    
-     return null;
- }
+  else if (
+    nSquare[0] == nSquare[3] &&
+    nSquare[3] == nSquare[6] &&
+    nSquare[0] != null
+  )
+    theWinner = nSquare[0];
+  else if (
+    nSquare[1] == nSquare[4] &&
+    nSquare[4] == nSquare[7] &&
+    nSquare[1] != null
+  )
+    theWinner = nSquare[1];
+  else if (
+    nSquare[2] == nSquare[5] &&
+    nSquare[5] == nSquare[8] &&
+    nSquare[2] != null
+  )
+    theWinner = nSquare[2];
+  //check diangonals [0, 4, 8] and [2, 4, 6]
+  else if (
+    nSquare[0] == nSquare[4] &&
+    nSquare[4] == nSquare[8] &&
+    nSquare[0] != null
+  )
+    theWinner = nSquare[0];
+  else if (
+    nSquare[2] == nSquare[4] &&
+    nSquare[4] == nSquare[6] &&
+    nSquare[2] != null
+  )
+    theWinner = nSquare[2];
+
+  if (theWinner != "") return theWinner;
+
+  return null;
+}
--- tik-tak-toe/src/Square.jsx
+++ tik-tak-toe/src/Square.jsx
@@ -1,24 +1,19 @@
-import ClickSound from './sound/clickSound.mp3';
-import useSound from 'use-sound';;
+import ClickSound from "./sound/clickSound.mp3";
+import useSound from "use-sound";
 
-export default function Square({value, onSquareClick}) {
+export default function Square({ value, onSquareClick }) {
+  const [playActive] = useSound(ClickSound, { volume: 0.75 });
 
-
-    const [playActive] = useSound(
-      ClickSound,
-      { volume: 0.75 }
-    );
-  
-  
-    return (
+  return (
     <>
-       
-      <button onMouseDown={playActive} className="square border border-5 border-white text-bg-dark" id="sqrButton" onClick={onSquareClick}>
-      {value}
-  
-    </button>
+      <button
+        onMouseDown={playActive}
+        className="square border border-5 border-white text-bg-dark"
+        id="sqrButton"
+        onClick={onSquareClick}
+      >
+        {value}
+      </button>
     </>
-  
-    
-    );
-  }
+  );
+}
